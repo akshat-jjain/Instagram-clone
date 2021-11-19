892aa0dd0046f0e80c43e7fa7fd93bc3b89ca1b8
Akshat Jain <akshat9983@gmail.com>
2021-11-19 22:27:32 +0530

    Update suggestions.js

--- assets/suggestions.js
+++ assets/suggestions.js
@@ -1,4 +1,14 @@
 const suggestions = [
-{
-},
+    {
+        username: "cyrus_real",
+        followed_by: "officialdoodles"
+    },
+    {
+        username: "user123",
+        followed_by: "niecegirl"
+    },
+    {
+        username: "diva_kaur",
+        followed_by: "niecegirl"
+    }
 ];
