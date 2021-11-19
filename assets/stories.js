825c2a0808186fe2ea9878bca785e79b949b817e
Akshat Jain <akshat9983@gmail.com>
2021-11-19 22:27:11 +0530

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -1,5 +1,17 @@
 const stories = [
-{
-
-},
+    {
+        username: "officialdoodles",
+        seen: "unseen",
+        link: "#"
+    },
+    {
+        username: "wrinkles_ss",
+        seen: "unseen",
+        link: "#"
+    },
+    {
+        username: "niecegirl",
+        seen: "seen",
+        link: "#"
+    },
 ];
