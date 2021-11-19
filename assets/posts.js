a474c62411e3b6c4c33d33641d0df88857cf4ac6
Akshat Jain <akshat9983@gmail.com>
2021-11-19 22:28:16 +0530

    Update posts.js

--- assets/posts.js
+++ assets/posts.js
@@ -1,5 +1,47 @@
 const posts = [
-{
-
-},
+    {
+        username: "officialdoodles",
+        posts_img: "https://jooinn.com/images/portrait-119.jpg",
+        caption: "Throw sass around like confetti.",
+        likes: 5000,
+        comments: 693,
+        post_time: "2021-11-19 21:44:10",
+        link: "#"
+    },
+    {
+        username: "niecegirl",
+        posts_img: "https://www.psdstack.com/wp-content/uploads/2019/08/copyright-free-images-750x420.jpg",
+        caption: "Smile a little more, regret a little less.",
+        likes: 4920,
+        comments: 1222,
+        post_time: "2021-11-19 21:36:10",
+        link: "#"
+    },
+    {
+        username: "wrinkles_ss",
+        posts_img: "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        caption: "Life is simple. It’s just not easy.",
+        likes: 920,
+        comments: 193,
+        post_time: "2021-11-19 22:05:10",
+        link: "#"
+    },
+    {
+        username: "officialdoodles",
+        posts_img: "https://media.istockphoto.com/photos/colorful-panoramic-mountain-view-at-sunrise-picture-id1129473522?k=20&m=1129473522&s=612x612&w=0&h=hkdf6tP6u_qETGVjHZXZ_6GLydngH88D2KesGtzJZJU=",
+        caption: "Being happy never goes out of style.",
+        likes: 1320,
+        comments: 493,
+        post_time: "2021-11-19 02:05:10",
+        link: "#"
+    },
+    {
+        username: "wrinkles_ss",
+        posts_img: "https://media.istockphoto.com/photos/magical-fairytale-forest-picture-id1082411378?k=20&m=1082411378&s=612x612&w=0&h=GalnNnhN5wVYqKXee-qy8AYrdOUDxEnkJlqC8T0q9dY=",
+        caption: "Grow through what you go through.",
+        likes: 12920,
+        comments: 1293,
+        post_time: "2021-11-18 22:05:10",
+        link: "#"
+    },
 ];
