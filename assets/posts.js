c2e0ee5f2c2a0ed25090e457fa9411a3480eb4d9
Akshat Jain <akshat9983@gmail.com>
2021-11-28 17:53:06 +0530

    Update posts.js

--- assets/posts.js
+++ assets/posts.js
@@ -1,45 +1,55 @@
 const posts = [
     {
+        id: "AA3xVFYerD",
         username: "officialdoodles",
         posts_img: "https://jooinn.com/images/portrait-119.jpg",
         caption: "Throw sass around like confetti.",
         likes: 5000,
+        liked_by: "boysideboard",
         comments: 693,
         post_time: "2021-11-19 21:44:10",
         link: "#"
     },
     {
+        id: "AA3xVFYerE",
         username: "niecegirl",
         posts_img: "https://www.psdstack.com/wp-content/uploads/2019/08/copyright-free-images-750x420.jpg",
         caption: "Smile a little more, regret a little less.",
         likes: 4920,
+        liked_by: "officialdoodles",
         comments: 1222,
         post_time: "2021-11-19 21:36:10",
         link: "#"
     },
     {
+        id: "AA3xVFYerF",
         username: "wrinkles_ss",
         posts_img: "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
         caption: "Life is simple. It’s just not easy.",
         likes: 920,
+        liked_by: "niecegirl",
         comments: 193,
         post_time: "2021-11-19 22:05:10",
         link: "#"
     },
     {
+        id: "AA3xVFYerH",
         username: "officialdoodles",
         posts_img: "https://media.istockphoto.com/photos/colorful-panoramic-mountain-view-at-sunrise-picture-id1129473522?k=20&m=1129473522&s=612x612&w=0&h=hkdf6tP6u_qETGVjHZXZ_6GLydngH88D2KesGtzJZJU=",
         caption: "Being happy never goes out of style.",
         likes: 1320,
+        liked_by: "cyrus_real",
         comments: 493,
         post_time: "2021-11-19 02:05:10",
         link: "#"
     },
     {
+        id: "AA3xVFYerZ",
         username: "wrinkles_ss",
         posts_img: "https://media.istockphoto.com/photos/magical-fairytale-forest-picture-id1082411378?k=20&m=1082411378&s=612x612&w=0&h=GalnNnhN5wVYqKXee-qy8AYrdOUDxEnkJlqC8T0q9dY=",
         caption: "Grow through what you go through.",
         likes: 12920,
+        liked_by: "officialdoodles",
         comments: 1293,
         post_time: "2021-11-18 22:05:10",
         link: "#"
