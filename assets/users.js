e831e4817df9fb2886628c850048a22a4abd95fd
Akshat Jain <akshat9983@gmail.com>
2021-11-28 17:52:34 +0530

    Update users.js

--- assets/users.js
+++ assets/users.js
@@ -1,8 +1,8 @@
 const users = {
     officialdoodles: {
         id: 1,
-        pic: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        name: "Doodle ",
+        pic: "//images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Emma Daizy ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -11,8 +11,8 @@ const users = {
     },
     cyrus_real: {
         id: 2,
-        pic: "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-        name: "Doodle ",
+        pic: "//images.unsplash.com/photo-1506863530036-1efeddceb993?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Olivia Cyrus ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -21,7 +21,8 @@ const users = {
     },
     wrinkles_ss: {
         id: 9,
-        pic: "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80", name: "Doodle ",
+        pic: "//images.unsplash.com/photo-1558507652-2d9626c4e67a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Ruby Iris ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -30,8 +31,8 @@ const users = {
     },
     niecegirl: {
         id: 12,
-        pic: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-        name: "Doodle ",
+        pic: "//images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Niece Addison",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -40,8 +41,8 @@ const users = {
     },
     boysideboard: {
         id: 52,
-        pic: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg",
-        name: "Doodle ",
+        pic: "//img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg",
+        name: "William James ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -51,7 +52,7 @@ const users = {
     user123: {
         id: 32,
         pic: "./assets/images/default-user.png",
-        name: "Doodle ",
+        name: "Gaurin Grover ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -60,8 +61,8 @@ const users = {
     },
     cousinlove: {
         id: 18,
-        pic: "https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=",
-        name: "Doodle ",
+        pic: "//media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=",
+        name: "Amelia ",
         posts: 100,
         followers: 100000,
         following: 25000,
@@ -70,8 +71,8 @@ const users = {
     },
     diva_kaur: {
         id: 15,
-        pic: "https://img.freepik.com/free-photo/portrait-confident-beautiful-brunette-woman-turning-face-camera-with-dreamy-look-white_1258-19144.jpg?size=626&ext=jpg",
-        name: "Doodle ",
+        pic: "//img.freepik.com/free-photo/portrait-confident-beautiful-brunette-woman-turning-face-camera-with-dreamy-look-white_1258-19144.jpg?size=626&ext=jpg",
+        name: "Diva Kaur ",
         posts: 100,
         followers: 100000,
         following: 25000,
