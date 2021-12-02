450d13651ff5bec41804edbcdfbe4f18ca001394
Akshat Jain <akshat9983@gmail.com>
2021-12-02 20:24:30 +0530

    Update posts.js

--- assets/posts.js
+++ assets/posts.js
@@ -1,6 +1,5 @@
-const posts = [
-    {
-        id: "AA3xVFYerD",
+const posts = {
+    AA3xVFYerD: {
         username: "officialdoodles",
         posts_img: "https://jooinn.com/images/portrait-119.jpg",
         caption: "Throw sass around like confetti.",
@@ -10,8 +9,7 @@ const posts = [
         post_time: "2021-11-19 21:44:10",
         link: "#"
     },
-    {
-        id: "AA3xVFYerE",
+    AA3xVFYerE: {
         username: "niecegirl",
         posts_img: "https://www.psdstack.com/wp-content/uploads/2019/08/copyright-free-images-750x420.jpg",
         caption: "Smile a little more, regret a little less.",
@@ -21,8 +19,7 @@ const posts = [
         post_time: "2021-11-19 21:36:10",
         link: "#"
     },
-    {
-        id: "AA3xVFYerF",
+    AA3xVFYerF: {
         username: "wrinkles_ss",
         posts_img: "https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
         caption: "Life is simple. It’s just not easy.",
@@ -32,8 +29,7 @@ const posts = [
         post_time: "2021-11-19 22:05:10",
         link: "#"
     },
-    {
-        id: "AA3xVFYerH",
+    AA3xVFYerH: {
         username: "officialdoodles",
         posts_img: "https://media.istockphoto.com/photos/colorful-panoramic-mountain-view-at-sunrise-picture-id1129473522?k=20&m=1129473522&s=612x612&w=0&h=hkdf6tP6u_qETGVjHZXZ_6GLydngH88D2KesGtzJZJU=",
         caption: "Being happy never goes out of style.",
@@ -43,8 +39,7 @@ const posts = [
         post_time: "2021-11-19 02:05:10",
         link: "#"
     },
-    {
-        id: "AA3xVFYerZ",
+    AA3xVFYerZ: {
         username: "wrinkles_ss",
         posts_img: "https://media.istockphoto.com/photos/magical-fairytale-forest-picture-id1082411378?k=20&m=1082411378&s=612x612&w=0&h=GalnNnhN5wVYqKXee-qy8AYrdOUDxEnkJlqC8T0q9dY=",
         caption: "Grow through what you go through.",
@@ -54,4 +49,5 @@ const posts = [
         post_time: "2021-11-18 22:05:10",
         link: "#"
     },
-];
+};
+const allPosts = Object.keys(posts);
