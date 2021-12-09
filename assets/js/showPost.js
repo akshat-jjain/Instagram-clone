fc1c322be403ed9f8021383d05f148d3ba17b638
Akshat Jain <akshat9983@gmail.com>
2021-12-09 22:50:04 +0530

    Update showPost.js

--- assets/js/showPost.js
+++ assets/js/showPost.js
@@ -1,2 +1,11 @@
-const getPost = "";
-document.title = "";
+const getPost = getParameter("p");
+$id("user-profile-pic").innerHTML = `
+<img src="${users[username].pic}" alt="${username}" class="profile-img">
+`;
+$id('username').innerHTML = username;
+$id('rp2').innerHTML = `<p><span>${numWithMil(users[username].posts)}</span> posts</p>
+<p><span>${numWithMil(users[username].followers)}</span> followers</p>
+<p><span>${numWithMil(users[username].following)}</span> following</p>`;
+$id('rp3').innerHTML = `${users[username].name}<br>${users[username].bio}`;
+
+document.title = `${posts[getPost].username}(@${}) • Instagram photos and videos`;
