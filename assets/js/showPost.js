d7e8ae776bf19517a919bcea5742fc12e8e6a055
Akshat Jain <akshat9983@gmail.com>
2021-12-11 23:05:27 +0530

    Update showPost.js

--- assets/js/showPost.js
+++ assets/js/showPost.js
@@ -1,8 +1,8 @@
 const getPost = getParameter("p");
 $id("user-profile-pic").innerHTML = `
-<img src="${users[username].pic}" alt="${username}" class="profile-img">
+<img src="${posts[getPost].pic}" alt="${username}" class="profile-img">
 `;
-$id('username').innerHTML = username;
+$id('username').innerHTML = posts[getPost].username;
 $id('rp2').innerHTML = `<p><span>${numWithMil(users[username].posts)}</span> posts</p>
 <p><span>${numWithMil(users[username].followers)}</span> followers</p>
 <p><span>${numWithMil(users[username].following)}</span> following</p>`;
